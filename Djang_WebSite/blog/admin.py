3eb541b3c401b43cbe9849acf000df37481c09c9
Yasmin Melo <carol721@gmail.com>
2024-04-10 17:12:03 -0400

    Correção

--- Djang_WebSite/blog/admin.py
+++ Djang_WebSite/blog/admin.py
@@ -7,6 +7,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status',)
     search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',Adi)}
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
